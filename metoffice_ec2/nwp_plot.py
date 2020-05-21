132e36662288776fe55dcf10e88b5708111e4ab1
Tom White <tom.e.white@gmail.com>
2020-05-21 14:39:28 +0100

    Use zipped Zarr file for test data

--- metoffice_ec2/nwp_plot.py
+++ metoffice_ec2/nwp_plot.py
@@ -6,14 +6,14 @@ from typing import Any, IO, List, Union
 import xarray as xr
 
 
-def find_zarr(fs: fsspec.spec.AbstractFileSystem, directory: str) -> List[str]:
+def find_zarr(fs: fsspec.spec.AbstractFileSystem, directory: str, suffix: str = ".zarr") -> List[str]:
     """Find all the zarr files in a filesystem directory"""
-    return [dir for dir in fs.listdir(directory, detail=False) if dir.endswith(".zarr")]
+    return [dir for dir in fs.listdir(directory, detail=False) if dir.endswith(suffix)]
 
 
 def filename_for_plot(directory, zarr_path: str, qualifier: str = "") -> str:
     """Get a suitable output filename for a plot produced from a Zarr path"""
-    basename = Path(zarr_path).stem
+    basename = Path(zarr_path).stem.replace(".zarr", "") # handle ".zarr" and ".zarr.zip"
     path = Path(directory, f"{basename}{qualifier}.png")
     return str(path)
 
--- metoffice_ec2/tests/nwp_plot_test.py
+++ metoffice_ec2/tests/nwp_plot_test.py
@@ -3,7 +3,7 @@ import imghdr
 from fsspec.implementations.local import LocalFileSystem
 from pathlib import Path
 import xarray as xr
-from zarr.storage import DirectoryStore
+from zarr.storage import ZipStore
 
 
 def test_local_plot_pipeline(tmp_path):
@@ -11,17 +11,17 @@ def test_local_plot_pipeline(tmp_path):
     fs = LocalFileSystem()
 
     # Find the first Zarr input file in the "data/mogreps" directory
-    listing = find_zarr(fs, "data/mogreps")
+    listing = find_zarr(fs, "data/mogreps", suffix=".zarr.zip")
     assert len(listing) == 1
     input_file = listing[0]
-    assert input_file.endswith("MOGREPS-UK__wind_from_direction__2020-03-15T15__2020-03-16T07.zarr")
+    assert input_file.endswith("MOGREPS-UK__wind_from_direction__2020-03-15T15__2020-03-16T07.zarr.zip")
 
     # Create a suitable filename for the output plot png file
     output_file = filename_for_plot(str(tmp_path), input_file, qualifier=f"_height{height}")
     assert output_file.endswith("MOGREPS-UK__wind_from_direction__2020-03-15T15__2020-03-16T07_height100.0.png")
 
     # Extract wind from direction array
-    store = DirectoryStore(input_file)
+    store = ZipStore(input_file)
     ds = xr.open_zarr(store)
     da = extract_wind_from_direction(ds, height)
     assert da.values.shape == (706, 553)
